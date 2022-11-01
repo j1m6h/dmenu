4a21b0193695108517ebe804df1e8da3abe1affd
Joshua <joshuamatthewhagen@yahoo.com>
2022-11-01 13:56:58 -0500

    Remove selection bg highlighting

--- config.h
+++ config.h
@@ -13,7 +13,7 @@ static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
  	[SchemeNorm] = { "#bbbbbb", "#222222" },
       	[SchemeSel] = { "#eeeeee", "#2A363B" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
+	[SchemeSelHighlight] = { "#ffc978", "#2A363B" },
 	[SchemeNormHighlight] = { "#ffc978", "#222222" },
         [SchemeOut] = { "#000000", "#00ffff" },
 };
